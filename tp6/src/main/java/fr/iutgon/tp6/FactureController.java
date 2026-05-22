9cbfab5d0800763cc5fa778e46627309f81de794
Noor Wallaert--Beaugendre <noorwallaert@gmail.com>
2026-05-22 11:27:10 +0200

    on peut ajouter des produits

--- tp6/src/main/java/fr/iutgon/tp6/FactureController.java
+++ tp6/src/main/java/fr/iutgon/tp6/FactureController.java
@@ -24,31 +24,61 @@ import javafx.util.StringConverter;
 import javafx.util.converter.IntegerStringConverter;
 
 import java.net.URL;
+import java.util.List;
 import java.util.Random;
 import java.util.ResourceBundle;
 
 public class FactureController implements Initializable {
-  public TableView<Ligne> table;
-  public TableColumn<Ligne, Integer> qte;
-  public TableColumn<Ligne, Produit> produit;
-  public TableColumn<Ligne, Number> prixUnitaire;
-  public TableColumn<Ligne, Number> totalHT;
-  public TableColumn<Ligne, Number> totalTTC;
-  public TextField sommeFacture;
+	public TableView<Ligne> table;
+	public TableColumn<Ligne, Integer> qte;
+	public TableColumn<Ligne, Produit> produit;
+	public TableColumn<Ligne, Number> prixUnitaire;
+	public TableColumn<Ligne, Number> totalHT;
+	public TableColumn<Ligne, Number> totalTTC;
+	public TextField sommeFacture;
 
-  /**
-   Called to initialize a controller after its root element has been completely processed.
+	/**
+	 * Called to initialize a controller after its root element has been completely
+	 * processed.
+	 * 
+	 * @param location  The location used to resolve relative paths for the root
+	 *                  object, or {@code null} if the location is not known.
+	 * @param resources The resources used to localize the root object, or
+	 *                  {@code null} if
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		qte.setCellValueFactory(new PropertyValueFactory<>("qte"));
+		
+		produit.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Ligne, Produit>, ObservableValue<Produit>>() {
+			public ObservableValue<Produit> call(TableColumn.CellDataFeatures<Ligne, Produit> cellData) {
+				return cellData.getValue().produitProperty();
+			}
+		});
+		
+		totalHT.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Ligne, Number>, ObservableValue<Number>>() {
+	        public ObservableValue<Number> call(TableColumn.CellDataFeatures<Ligne, Number> cellData) {
+	            return cellData.getValue().totalHTProperty();
+	        }
+	    });
+		
+		totalTTC.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Ligne, Number>, ObservableValue<Number>>() {
+	        public ObservableValue<Number> call(TableColumn.CellDataFeatures<Ligne, Number> cellData) {
+	            return cellData.getValue().totalTTCProperty();
+	        }
+	    });
+		
+	}
 
-   @param location  The location used to resolve relative paths for the root object, or
-   {@code null} if the location is not known.
-   @param resources The resources used to localize the root object, or {@code null} if
-   */
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    //TODO préparer la table
-  }
 
-  public void onAjouter(ActionEvent actionEvent) {
-    //TODO ajouter un produit aléatoire à la table
-  }
+	public void onAjouter(ActionEvent actionEvent) {
+		Random rand = new Random();
+		List<Produit> produits = FabriqueProduits.getProduits();
+		Produit produit = produits.get(rand.nextInt(produits.size()));
+
+		int qte = rand.nextInt();
+
+		Ligne ligne = new Ligne(qte, produit);
+		table.getItems().add(ligne);
+	}
 }
